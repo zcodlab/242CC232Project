197eeb5066ba9e065251fc7096fb800b90c2174e
zmamanir@uni.edu.pe <>
2024-09-02 09:56:06 -0500

    add Sort class

--- src/main/java/uni/aed/sort/Sort.java
+++ src/main/java/uni/aed/sort/Sort.java
@@ -2,6 +2,24 @@ package uni.aed.sort;
 
 public class Sort {
     private Integer[] Y;
+
+    public Sort(int N) {
+        this.Y = new Integer[N];
+    }
+
+    public void setY(Integer[] Y) {
+        this.Y = Y;
+    }    
+
+    @Override
+    public String toString() {
+        StringBuilder str=new StringBuilder();
+        for(Integer y:Y){
+            str.append(y.toString());
+        }
+        return str.toString();
+    }
+    
     
     public Integer[] selectionWuSort(){
         if(Y==null) return null;
