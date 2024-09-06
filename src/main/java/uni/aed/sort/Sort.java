48fe27680711c8858419a89fa59c37dac03a1f5e
zmamanir@uni.edu.pe <>
2024-09-06 10:38:25 -0500

    add bubbleWuSort algorithm

--- src/main/java/uni/aed/sort/Sort.java
+++ src/main/java/uni/aed/sort/Sort.java
@@ -42,4 +42,25 @@ public class Sort {
         return X;
     }
     
+    public Integer[] bubbleWuSort(){
+        if(Y==null) return null;
+        Integer[] X=Y.clone();
+        int temp,bottom,i;
+        boolean exchanged=true;
+        bottom=X.length-2;
+        while(exchanged){
+            exchanged=false;
+            for(i=0;i<=bottom;i++){
+                if(X[i]>X[i+1]){
+                    temp=X[i];
+                    X[i]=X[i+1];
+                    X[i+1]=temp;
+                    exchanged=true;                    
+                }
+            }
+            bottom--;
+        }
+        return X;
+    }
+    
 }
