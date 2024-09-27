91a3043cdbe0e0ae9319cf27c0fc712675d9d4c8
zmamanir@uni.edu.pe <>
2024-09-27 10:45:38 -0500

    add testOSimpleLinkedList method

--- src/main/java/uni/aed/simplelinkedlist/bicicleta/OSimpleLinkedListMain.java
+++ src/main/java/uni/aed/simplelinkedlist/bicicleta/OSimpleLinkedListMain.java
@@ -1,5 +1,8 @@
 package uni.aed.simplelinkedlist.bicicleta;
 
+import java.util.Scanner;
+import uni.aed.model.Bicicleta;
+
 public class OSimpleLinkedListMain {
     public static void main(String[] args){
         OSimpleLinkedListMain lista=new OSimpleLinkedListMain();
@@ -7,6 +10,35 @@ public class OSimpleLinkedListMain {
     }
     
     private void testOSimpleLinkedList(){
+        Scanner scr=new Scanner(System.in).useDelimiter("\n");
+        Bicicleta b;
+        String nombre;               
+        OSimpleLinkedList olista=new OSimpleLinkedList();
+        Bicicleta b1=new Bicicleta("Sonya Smith");
+        Bicicleta b2=new Bicicleta("Barack Obama");
+        Bicicleta b3=new Bicicleta("Kamala Harris");
+        Bicicleta b4=new Bicicleta("Dina Boluarte");
         
+        olista.addLast(b1);
+        olista.addLast(b2);
+        olista.addLast(b3);
+        olista.addLast(b4);
+        
+        System.out.println("Visualizando el contenido de la lista de Bicicletas:"+ olista.size());
+        System.out.println(olista.toString());
+        
+        System.out.println("Eliminando un elemento de la lista");
+        System.out.println("Consigne el nombre del propietario de la bicicleta a eliminar");
+        nombre=scr.next();
+        b=new Bicicleta(nombre);
+        olista.remove(b);
+        
+        System.out.println("Visualizando el contenido de la lista de Bicicletas:"+ olista.size());
+        System.out.println(olista.toString());
+        
+        System.out.println("Consigne el nombre del propietario de la bicicleta a buscar");
+        nombre=scr.next();
+        b=new Bicicleta(nombre);        
+        System.out.println("El nombre del propietario de la bicicleta se ubico en la posicion:" + olista.search(b));        
     }
 }
